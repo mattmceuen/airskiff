b8e0b012e7d070e35c9216a019acdbb2aeed4237
Matt McEuen <matt.mceuen@att.com>
2018-08-21 13:42:05 -0500

    Fix namespace in clouds.yaml keystone url
    Update the clouds.yaml auth_url from using an openstack
    namespace to referencing the correct ucp namespace.

--- tools/deployment/common/setup-client.sh
+++ tools/deployment/common/setup-client.sh
@@ -32,6 +32,6 @@ clouds:
       project_name: 'admin'
       project_domain_name: 'default'
       user_domain_name: 'default'
-      auth_url: 'http://keystone.openstack.svc.cluster.local/v3'
+      auth_url: 'http://keystone.ucp.svc.cluster.local/v3'
 EOF
 
